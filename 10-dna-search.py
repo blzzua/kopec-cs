20b85b113b4fd0eda4eca17e17006d11e7fe88ea
blzzua <blzzua@github.com>
2020-04-23 19:43:56 +0300

    элементарный поиск подпоследовательности в последовательности

---
+++ 10-dna-search.py
@@ -0,0 +1,32 @@
+from enum import IntEnum
+from typing import Tuple, List
+
+Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
+Gene = List[Codon]
+
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i+2) >= len(s):
+            return gene
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i+1]], Nucleotide[s[i+2]])
+        gene.append(codon)
+    return gene
+
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
+
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
+gene_str = """ACGTACGTATCGATCAGTCGTAGCTACAGGCTAGCTAGCTAGCTACGACGTA"""
+my_gene: Gene = string_to_gene(gene_str)
+
+print(acg)
+print(linear_contains(my_gene, acg))
+print(linear_contains(my_gene, gat))
+
